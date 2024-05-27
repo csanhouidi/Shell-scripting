42308c9307917a746b01b31ca186936ae2e64c1b
csanhouidi <126991271+csanhouidi@users.noreply.github.com>
2024-05-27 10:19:16 -0500

    Update deploy_web_app.sh

--- deploy_web_app.sh
+++ deploy_web_app.sh
@@ -12,3 +12,19 @@ sudo git clone https://github.com/csanhouidi/shell-scripting /var/www/html/shell
 #### Set up Nginx to serve the HTML file
 ```echo "Setting up Nginx to serve the HTML file..."
 sudo bash -c 'cat <<EOL > /etc/nginx/sites-available/simpleweb
+server {
+    listen 80;
+    server_name yourdomain.com;
+
+    root /var/www/html/shell-scripting/www;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+}
+EOL'
+```
+#### Enable the new Nginx configuration
+```echo "Enabling the new Nginx configuration..."
+sudo ln -s /etc/nginx/sites-available/simpleweb /etc/nginx/sites-enabled/
