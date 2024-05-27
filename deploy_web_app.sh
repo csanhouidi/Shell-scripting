d8e8a1b9fd078c54c64f7b433162a822513e78bc
csanhouidi <126991271+csanhouidi@users.noreply.github.com>
2024-05-27 10:18:01 -0500

    Update deploy_web_app.sh

--- deploy_web_app.sh
+++ deploy_web_app.sh
@@ -1,1 +1,14 @@
-
+#### Update package list and install necessary packages
+``` #!/bin/bash
+  - echo "Updating package list and installing necessary packages..."  
+  - sudo apt update
+  - sudo apt install -y nginx git
+```
+#### Clone the web application from GitHub
+```
+echo "Cloning the web application repository from GitHub..."
+sudo git clone https://github.com/csanhouidi/shell-scripting /var/www/html/shell-scripting
+```
+#### Set up Nginx to serve the HTML file
+```echo "Setting up Nginx to serve the HTML file..."
+sudo bash -c 'cat <<EOL > /etc/nginx/sites-available/simpleweb
